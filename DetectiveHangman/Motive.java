e82cefc7d9372e445358245d12a9915d0e595882
kiahm <martinkm10@vcu.edu>
2026-04-09 17:57:32 -0400

    edited Motive.java

--- DetectiveHangman/Motive.java
+++ DetectiveHangman/Motive.java
@@ -1,14 +1,25 @@
 package DetectiveHangman;
 /**
  * Description
- * @Author
- * @Version
+ * @author Kiah Martin
+ * @version 1.0
  */
 
 public enum Motive {
-    JEALOUSY,
-    REVENGE,
-    MONEY,
-    THRILL,
-    FAME
+    JEALOUSY ("Jealousy"),
+    REVENGE ("Revenge"),
+    MONEY("Money"),
+    THRILL("Thrill"),
+    FAME("Fame");
+
+    private String displayName;
+
+    Motive(String displayName) {
+        this.displayName = displayName;
+
+    }
+    public String getDisplayName() {
+        return displayName;
+    }
+
 }
