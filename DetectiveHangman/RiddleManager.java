b8dbf98cd84c3f78fa59152a587513e9cdfae380
First Last <nevaehcode@gmail.com>
2026-04-13 22:53:10 -0400

    save my changes to txt files and riddle classes before pulling
    # Conflicts:
    #	out/production/Detective-Hangman-CMSC255-/.idea/misc.xml
    #	out/production/Detective-Hangman-CMSC255-/DetectiveHangman/LocationRiddles.txt
    #	out/production/Detective-Hangman-CMSC255-/DetectiveHangman/MotiveRiddles.txt
    #	out/production/Detective-Hangman-CMSC255-/DetectiveHangman/MurdererRiddles.txt
    #	out/production/Detective-Hangman-CMSC255-/DetectiveHangman/WeaponRiddles.txt

--- DetectiveHangman/RiddleManager.java
+++ DetectiveHangman/RiddleManager.java
@@ -5,45 +5,113 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// note from max: more information about class in doc, check it out
-// if confused, lowk i'm just barely putting these classes together rn
+
 /**
- * This class loads the riddles.txt file,
- * and parses it based on a chosen component type,
- * then saves the three parsed lines as objects in the ArrayList
+ *
  *
  * @author Nevaeh Dickerson
  * @version
  */
 public class RiddleManager {
     //data fields
-    private File input;
+    private File locationRiddles;
+    private File motiveRiddles;
+    private File murdererRiddles;
+    private File victimRiddles;
+    private File weaponRiddles;
+
     private RiddleComponent componentCategory;
-    private ArrayList<Riddle> riddleOptions;
+//    private ArrayList<Riddle> riddleOptions;
 
     // constructor
-    public RiddleManager(File input, RiddleComponent componentCategory) {
-        this.input = input;
+    public RiddleManager(File locationRiddles, File motiveRiddles, File murdererRiddles, File victimRiddles, File weaponRiddles, RiddleComponent componentCategory) {
+        this.locationRiddles = locationRiddles;
+        this.motiveRiddles = motiveRiddles;
+        this.murdererRiddles = murdererRiddles;
+        this.victimRiddles = victimRiddles;
+        this.weaponRiddles = weaponRiddles;
         this.componentCategory = componentCategory;
-        this.riddleOptions = new ArrayList<>();
+//       /  this.riddleOptions = new ArrayList<>();
     }
 
-    public void parseTxtFile() {
-        //TODO take in text file in parameter
-        try{
-            Scanner inputFile = new Scanner(input);
+
+    /**
+     * This method loads all the riddles txt files,
+     * and parses it based on a chosen component type,
+     * then saves the three parsed lines as objects in the ArrayList
+     *
+     * @param inputFile, Scanner
+     * @return lines, ArrayList<String>
+     *
+     */
+    public ArrayList<String> getRiddleOptions(Scanner inputFile) {
+
+        //TODO these Scanner objects should be created in the main and used as arguments in this method
+        //handles the locationRiddles file
+//        Scanner locationFile = openFile("locationRiddles", locationRiddles);
+//        Scanner motiveFile = openFile("motiveRiddles", motiveRiddles);
+//        Scanner murdererFile = openFile("murdererRiddles", murdererRiddles);
+//        Scanner victimFile = openFile("victimRiddles", victimRiddles);
+//        Scanner weaponFile = openFile("weaponRiddles", weaponRiddles);
+
+        int statingLine = 1;
+        int count = 1;
+        int component = (int)(Math.random() * 5) + 1; //generates a random num from 1-5 inclusive
+
+        if (component == 2){
+            statingLine = 5;
+        } else if(component == 3){
+            statingLine = 9;
+        }else if(component == 4){
+            statingLine = 13;
+        }else if(component == 5){
+            statingLine = 17;
+        }
+
+        //Moves Scanner curso to the appropriate start of the file
+        //depending on what component option is randomly chose
+        ArrayList<String> lines = new ArrayList<>();
+        while(inputFile.hasNextLine()){
+            count++;
+            // TODO: split component category by chosen element
+            if (count == (statingLine -1)){
+               break;
+            }
+        }
+
+        //adds the three riddle options for a randomized component selection
+        //as strings into the arrayList lines
+        for(int i = 0; i < 3; i++){
+            lines.add(inputFile.next());
+        }
+
+        return lines;
+    }
+
+    /**
+     * This is a helper method to the parseTxtFile().
+     * It opens the passed in file within a try-catch block
+     * and outputs a message that conveys if the file was able to be
+     * opened or not.
+     *
+     * @param fileName, string
+     * @param input, the specific component riddles txt file
+     * @return inputFile, scanner object for the passed in file
+     */
+    public Scanner openFile(String fileName, File input) {
+
+        Scanner inputFile = null;
+        try {
+            inputFile = new Scanner(input);
+            System.out.print(fileName + " file opened successfully");
+
         } catch (FileNotFoundException e) {
             Scanner newFile = new Scanner(System.in);
-            System.out.println("File Not Found. Please enter a valid file.");
+            System.out.println(fileName + " Not Found. Please enter a valid " + fileName + " file.");
 
             input = new File(newFile.nextLine());
         }
 
-        // TODO: split data into component categories
-
-
-        // TODO: split component category by chosen element
-        // TODO: loop through the chosen element and create an object for each line
-        // TODO: add the objects into the arraylist (there should be three objects in arraylist)
+        return inputFile;
     }
 }
